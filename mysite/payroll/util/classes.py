850ebb0ba4b1eca4cb4f5c016df950647884e196
Xinyu Hou <xinyu@boltonsoftware.com>
2018-05-24 12:16:33 -0400

    Implement Payperiod

--- mysite/payroll/util/classes.py
+++ mysite/payroll/util/classes.py
@@ -1,7 +1,25 @@
+from calendar import monthrange
+import datetime
+
 class PayPeriod:
     def __init__(self, date):
-        self.date = date
-        # return a date period, [01/mm/yy - 15/mm/yy] or [16/mm/yy - last_day/mm/yy]
+        year = date.year
+        month = date.month
+        day = date.day
+        first_day, last_day = monthrange(year, month)
+        if (day <= 15):
+            self.start_date = datetime.date(year, month, 1)
+            self.end_date = datetime.date(year, month, 15)
+        else:
+            self.start_date = datetime.date(year, month, 16)
+            self.end_date = datetime.date(year, month, last_day)
+
+    def __str__(self):
+        return self.to_str()
+
+    def to_str(self):
+        # return a date period, mm/01/yy - mm/15/yy or mm/16/yy - mm/last_day/yy
+        return self.start_date.strftime('%m/%d/%Y') + ' - ' + self.end_date.strftime('%m/%d/%Y')
 
 class ReportParser:
     def __init__(self, report):
