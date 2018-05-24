c1f0a034d11b0cc2271ac7d5f2b6e5953becafac
Xinyu Hou <xinyu@boltonsoftware.com>
2018-05-23 20:57:13 -0400

    Create some RESTful endpoints

--- mysite/payroll/urls.py
+++ mysite/payroll/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('employees', views.all_payroll, name='all_payroll'),
+    path('employees/<int:employee_id>/', views.individual_payroll, name='individual_payroll'),
+    path('report/', views.upload_report, name='upload_report'),
 ]
--- mysite/payroll/views.py
+++ mysite/payroll/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
+from .models import Payroll
 
+def all_payroll(request):
+    first_five_payroll = Payroll.objects.order_by('employee')[:5]
+    output = ', '.join([str(p.employee) for p in first_five_payroll])
+    return HttpResponse(output)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
+    #return HttpResponse("Welcome to this new payroll system!")
+
+def individual_payroll(request, employee_id):
+    return HttpResponse("Welcome back user %s!" % employee_id)
+
+def upload_report(request):
+	return HttpResponse("Please select a report file you want to upload!")
