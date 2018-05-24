85a8a01a7030f3f7155511b16f2deee848a2618b
Xinyu Hou <xinyu@boltonsoftware.com>
2018-05-23 20:11:47 -0400

    Initial model design for payroll, payslip and jobgroup

--- mysite/payroll/models.py
+++ mysite/payroll/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class JobGroup(models.Model):
+    group = models.CharField(max_length = 5)
+    rate = models.IntegerField(default = 0)
+
+class Payslip(models.Model):
+    pay_date = models.DateField('Paid Date')
+    hours = models.IntegerField(default = 0)
+    employee = models.IntegerField(default = 0)
+    job_group = models.ForeignKey(JobGroup, on_delete = models.PROTECT)
+
+class Payroll(models.Model):
+	employee = models.IntegerField(default = 0)
+	pay_period = models.DateTimeField('Paid Date')
+	amount = models.IntegerField(default = 0)
+	
