c2f7114d80a54abaecff44e6ea7428df0a25c3c1
Xinyu Hou <xinyu@boltonsoftware.com>
2018-05-24 11:00:58 -0400

    Use template to render payroll views

--- mysite/payroll/urls.py
+++ mysite/payroll/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'payroll'
+
 urlpatterns = [
-    path('employees', views.all_payroll, name='all_payroll'),
+    path('employees/', views.all_payroll, name='all_payroll'),
     path('employees/<int:employee_id>/', views.individual_payroll, name='individual_payroll'),
-    path('report/', views.upload_report, name='upload_report'),
+    path('admin/', views.admin, name='admin'),
+    path('report/', views.report, name='report'),
 ]
--- mysite/payroll/views.py
+++ mysite/payroll/views.py
@@ -1,15 +1,34 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+import datetime
+
 from .models import Payroll
+from payroll.util.classes import PayPeriod
 
 def all_payroll(request):
-    first_five_payroll = Payroll.objects.order_by('employee')[:5]
-    output = ', '.join([str(p.employee) for p in first_five_payroll])
-    return HttpResponse(output)
+    first_five_payroll = Payroll.objects.order_by('employee')
+    context = {
+        'title': "Welcome to this new payroll system!",
+        'payrolls': first_five_payroll,
+    }
 
-    #return HttpResponse("Welcome to this new payroll system!")
+    return render(request, 'payroll/payroll.html', context)
 
 def individual_payroll(request, employee_id):
-    return HttpResponse("Welcome back user %s!" % employee_id)
+    individual_payrolls = list(Payroll.objects.filter(employee = employee_id))
 
-def upload_report(request):
-	return HttpResponse("Please select a report file you want to upload!")
+    context = {
+        'title': "Welcome back user %s!" % employee_id,
+        'payrolls': individual_payrolls,
+    }
+    return render(request, 'payroll/payroll.html', context)
+
+def admin(request):
+    context = {
+        'title': "Please select a report file you want to upload!",
+    }
+
+    return render(request, 'payroll/admin.html', context)
+
+def report(request):
+    return redirect('payroll:all_payroll')
